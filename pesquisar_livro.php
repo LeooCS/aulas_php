6235d5b639a918e15ac72f1b26ef45ae99a62dbe
LeooCS <leonardocs0630@gmail.com>
2022-03-28 07:41:28 -0300

    Envio 28/03/2022

--- pesquisar_livro.php
+++ pesquisar_livro.php
@@ -3,13 +3,13 @@
 
     $nome_livro = $_POST['c_nome_livro'];
 
-    $sql = "SELECT `codigo_item` FROM `item` WHERE descricao = $nome_livro";
+    $sql = "SELECT `codigo_item` FROM `item` WHERE descricao = '$nome_livro'";
 
     $resultado = mysqli_query($conexao, $sql);
 
-    $quantidade = mysql_num_rows($resultado);
+    $quantidade = mysqli_num_rows($resultado);
 
-    echo $resultado;
+    echo "Existem ".$quantidade."de livro(s) cadastrado(s) com o nome de ".$nome_livro;
 
 ?>
 <p><a href="form_cadastrar_livro.html">Voltar</a
